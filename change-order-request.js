4403e03afcd0d1a3c21462150db7d40e7dc1eb35
SwiftDragon96 <87925917+SwiftDragon96@users.noreply.github.com>
2022-11-13 01:34:31 -0500

    Add files via upload

---
+++ change-order-request.js
@@ -0,0 +1,36 @@
+//alert
+alert("Warning! This page is for Mission Control Personnel Only!")
+// functions
+function countdown() 
+{
+    // variable, we need a countdown timer
+    var currTime=10;
+    //the main loop for the timeout function
+    for(var i=0; i<=11; i++)
+    {
+        if (i == 11) 
+        {
+            setTimeout(function ()
+            {
+                //code goes here for timer
+                document.getElementById("countdownDisplay").innerHTML = "Blastoff!!!";
+            }, 1000 * i);
+        } 
+        else if (i > 5)
+        {
+            setTimeout(function ()
+            {
+                document.getElementById("countdownDisplay").innerHTML = "Warning Less than 1/2 way to launch, time left = " + currTime + " seconds";
+                currTime = currTime - 1;
+            }, 1000 * i);
+        }
+        else 
+        {
+            setTimeout(function ()
+            {
+                document.getElementById("countdownDisplay").innerHTML = currTime + " seconds remaining";
+                currTime = currTime - 1;
+            }, 1000 * i);
+        }
+    }
+}
